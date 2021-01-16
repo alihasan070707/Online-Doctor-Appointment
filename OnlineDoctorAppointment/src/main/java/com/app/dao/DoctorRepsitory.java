cca5a7cbc40b53a964a694c7af2ba7419ecf6f1c
ali hasan <alihasansyed8@gmail.com>
2021-01-16 16:58:54 +0530

    Added doctor service

---
+++ OnlineDoctorAppointment/src/main/java/com/app/dao/DoctorRepsitory.java
@@ -0,0 +1,11 @@
+package com.app.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.pojos.Doctor;
+
+public interface DoctorRepsitory extends JpaRepository<Doctor, Integer>{
+	
+		
+		
+}
