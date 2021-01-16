290318ba5c3c31bc5a27d681b5eed6167187100a
Home <Home@DESKTOP-D1HF153>
2021-01-16 22:44:44 +0530

    Updated dao

--- OnlineDoctorAppointment/src/main/java/com/app/dao/PatientRepository.java
+++ OnlineDoctorAppointment/src/main/java/com/app/dao/PatientRepository.java
@@ -5,19 +5,23 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.app.pojos.Appointment;
 import com.app.pojos.Patient;
+import com.app.pojos.Prescription;
 
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 	Optional<Patient> findById(Integer patient_id);
 	
-	Optional<Patient> findByEmailAndPassword(String email, String password);
+	Optional<Patient> findByEmailAndPassword(String email, char[] password);
 	
 	@Query("Select app from Appointment app where app.patient_id = ?1")
 	List<Appointment> findAllAppointments(Integer patient_id);
 	
 	@Query("Select pres from Prescription pres where pres.patient_id = ?1")
-	List<Appointment> findAllPrescriptoins(Integer patient_id);
+	List<Prescription> findAllPrescriptions(Integer patient_id);
 	
+	@Query(value = "delete from Appointment a where a.appointment_id = :appointmentId")
+	void cancelAppointment(@Param("appointmentId") Integer appointmentId);
 }
