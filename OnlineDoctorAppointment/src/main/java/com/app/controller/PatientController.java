2e198bad9744a7590d4ce082c8369131bb1e761c
Sparsh <sparsh759@gmail.com>
2021-01-17 17:17:46 +0530

    something

--- OnlineDoctorAppointment/src/main/java/com/app/controller/PatientController.java
+++ OnlineDoctorAppointment/src/main/java/com/app/controller/PatientController.java
@@ -1,6 +1,60 @@
 package com.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.app.pojos.Patient;
+import com.app.pojos.Prescription;
+import com.app.service.IPatientService;
+
+@RestController
+@RequestMapping("/patient")
 public class PatientController {
 	
-
+	@Autowired
+	private IPatientService service;
+	
+	@GetMapping("/login")
+	public String showPatientLoginPage() {
+		return "/login";
+	}
+	
+	@PostMapping("/login")
+	public String loginPatient(@RequestParam String email, @RequestParam String password) {
+		Patient patient = service.getPatientDetails(email, password.toCharArray());
+		if (patient == null) {
+			return "/login";
+		}
+		return "/somePage";  //need to be created
+	}
+	
+	@GetMapping("/register")
+	public String showPatientRegisterPage() {
+		return "/register";
+	}
+	
+	@PostMapping("/register")
+	public String registerPatient(@Validated Patient patient) {
+		if(service.addPatient(patient)) {
+			return "/somePage"; //need to be created
+		}
+		return "/register";
+	}
+	
+	@GetMapping("prescriptions")
+	public String getPrescriptions(@RequestParam Integer patientId, Model map) {
+		List<Prescription> list = service.getPrescription(patientId);
+		map.addAttribute("prescriptionList", list);
+		if(list == null) {
+			return "/";
+		}
+		return "/somePage"; //need to be created
+	}
 }
