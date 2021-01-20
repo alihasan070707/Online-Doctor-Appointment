e75e6661b1dd8d5cf940ae760b013cebae4a9b5e
Sparsh <sparsh759@gmail.com>
2021-01-20 13:40:19 +0530

    created profile component

---
+++ FrontEnd/OnlineDoctorAppointment/src/app/patient-profile/patient-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-profile',
+  templateUrl: './patient-profile.component.html',
+  styleUrls: ['./patient-profile.component.css']
+})
+export class PatientProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
